c6375d2c31c619bd1ffcf52a2d1af974e726e13f
WNTIV <wntivc@gmail.com>
2021-08-18 09:12:55 +1200

    Commented my code :)

--- IO Game/Game.cs
+++ IO Game/Game.cs
@@ -6,10 +6,14 @@ using System.Collections.Generic;
 
 namespace IO_Game
 {
+    // Represents all the gamemodes
     public static class Gamemodes
     {
+        // Free for all
         static Gamemode FFA = new Gamemode("FFA");
+        // Capture the flag
         static Gamemode CTF = new Gamemode("CTF");
+        // Represents a single Gamemode
         public class Gamemode
         {
             private readonly string gamemode;
@@ -19,122 +23,188 @@ namespace IO_Game
             }
         }
     }
+    // Represents a game/arena
     public class Game
     {
+        // Represents an item of some sort
         public class Item
         {
             //...
         }
+        // Represents the shop in any given game
         private class Shop
         {
+            // Represents an item in the shop
             public class ShopItem
             {
+                // The price of the item, in a class Heavy
                 private Heavy price;
+                // The item, as a class Item
                 private Item item;
+                // Construct the variables
                 public ShopItem(Heavy p, Item i)
                 {
                     price = p;
                     item = i;
                 }
+                // Buy an item
                 public Item Buy(Heavy budget)
                 {
+                    // Can we afford?
                     if(budget.Compare(price) != "<")
                     {
+                        // Remove total sum of owed money from our budget
                         budget.Subtract(price);
+                        // And return the item
                         return item;
                     }
+                    // Otherwise shout at people
                     throw new Exception("CANNOT_AFFORD");
                 }
             }
+            // List of all the shop's items
             private List<ShopItem> shopItems = new List<ShopItem>();
+            // Functionality to add MORE items
             public void AddItem(ShopItem item) => shopItems.Add(item);
         }
+        // Represents a player
         private class Player
         {
-            private class Position
-            {
-                private float x;
-                private float y;
-                public Position()
-                {
-                    x = 0;
-                    y = 0;
-                }
-                public Position(float X, float Y)
-                {
-                    x = X;
-                    y = Y;
-                }
-            }
+            // The SocketHandler() reference we're using
             private Task socketHandler;
+            // When we create a player, they need to have a connected WebSocket
             public Player(WebSocket s)
             {
+                // Snag that Sock(et)
                 socket = s;
+                // And start up our listener
                 socketHandler = SocketHandler();
             }
+            // The stolen Sock(et)
             private readonly WebSocket socket;
+            // This players weight as a Heavy
             private Heavy size = new Heavy();
-            private Dictionary<int, Item> inventory = new Dictionary<int, Item>();
+            // What do they have? in [slot name, item] forms
+            // slot names such as "equipped.0, equipped.13, inventory.0, inventory.14"
+            private Dictionary<string, Item> inventory = new Dictionary<string, Item>();
+            // We need to listen to the user so we know what they're doing...
             private async Task SocketHandler()
             {
+                // Store the result of the socket
                 var buffer = new byte[1024 * 4];
+                // Wait for the result
                 WebSocketReceiveResult result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
                 while (!result.CloseStatus.HasValue)
                 {
+                    // Send it right on back (for the moment)
                     await socket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
 
+                    // Wait for the next result
                     result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
                 }
+                // If you die, I die too.
                 await socket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
             }
         }
+        // Represents a position on the game's map
+        private class Position
+        {
+            // X pos
+            private float x;
+            // Y pos
+            private float y;
+            // Construct with no params
+            public Position()
+            {
+                x = 0;
+                y = 0;
+            }
+            // Construct with pre-set numbers
+            public Position(float X, float Y)
+            {
+                x = X;
+                y = Y;
+            }
+        }
+        // Game ID
         public readonly ID Id;
+        // This game's gamemode
         public readonly Gamemodes.Gamemode gamemode;
+        // This game's shop
         private Shop shop = new Shop();
+        // List of all players
         private List<Player> players = new List<Player>();
+        // Probable do something on construction
         public Game() { 
             
         }
+        // If we want to construct a game of a certain gamemode
         public Game(Gamemodes.Gamemode mode)
         {
             gamemode = mode;
         }
+        // Functionality to add a player to the game
         public void AddPlayer(WebSocket socket)
         {
             players.Add(new Player(socket));
         }
     }
+    // Represents a game ID
     public class ID
     {
+        // The ID
         readonly string Id;
+        static List<string> reserved = new List<string>();
+        static Random random = new Random();
+        // Create a unique id
         public ID()
         {
-            Id = "";
+            // Just hope we dont have more games than random values in the world.
+            Id = random.Next().ToString();
+            while (reserved.Contains(Id))
+            {
+                // Otherwise we're f***ed
+                Id = random.Next().ToString();
+            }
+            reserved.Add(Id);
         }
+        // Is this ID the same as that 'ID'?
         public bool Matches(string id)
         {
+            // Maybe?
             return Id == id;
         }
+        // Is this ID the same as that ID :)?
         public bool Matches(ID id)
         {
+            // IDK, ask him... lol
             return id.Matches(Id);
         }
     }
+    // We need somewhere to store everything...
     public static class Server
     {
+        //All the games we -can- (might be able to) join
         private static List<Game> games = new List<Game>();
-        public static bool CanJoin(ID gameID) { return true; }
-        public static bool CanJoin(string gameId) { return true; }
+        // Can we join the game?
+        public static bool CanJoin(ID gameID) { /*Yes, for now...*/ return true; }
+        public static bool CanJoin(string gameId) { /*Yes, for now...*/ return true; }
+        // Does this game even exist???
         public static bool GameExists(ID gameID) {
+            // Loop through all the games
             for(var i = 0; i < games.Count; i++)
             {
+                // Oh yeah, that's why I needed the clas ID.Matches() function
                 if (games[i].Id.Matches(gameID))
                 {
+                    // Yes, that would be me
                     return true;
                 }
             }
+            // No, noone here likes him...
             return false;
         }
+        // It's the EXACT same f***ing thing
         public static bool GameExists(string gameID)
         {
             for (var i = 0; i < games.Count; i++)
@@ -146,19 +216,28 @@ namespace IO_Game
             }
             return false;
         }
+        // Find me a game to play PLEASEEEEEEEEEEEEEEE...
         public static ID FindGame()
         {
+            // Meet all our games
             for(var i = 0; i < games.Count; i++)
             {
+                // This is {{games[i].name}}...
+                // And he says...
                 if(CanJoin(games[i].Id))
                 {
+                    // "You CAN play with me"
                     return games[i].Id;
                 }
+                // No, Im a sad'o who bullies people and loves exclusion
             }
+            // Im sorry, but noone likes you
+            // I'll genetically grow someone who does
             Game g = new Game();
             games.Add(g);
             return g.Id;
         }
+        // Do the same hexing thing but this time we are the discriminatory one.
         public static ID FindGame(Gamemodes.Gamemode gamemode)
         {
             for (var i = 0; i < games.Count; i++)
@@ -172,8 +251,10 @@ namespace IO_Game
             games.Add(g);
             return g.Id;
         }
+        // Get the Game object based upon it's ID
         public static Game GetGame(ID gameID)
         {
+            // I cant be bothered commenting this.
             for (var i = 0; i < games.Count; i++)
             {
                 if (games[i].Id.Matches(gameID))
--- IO Game/Startup.cs
+++ IO Game/Startup.cs
@@ -1,105 +1,96 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net.WebSockets;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace IO_Game
 {
+    // What to do on server startup...
     public class Startup
     {
+        // Something that looks pretty useful. But IDK.
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        // Stores all the config values???
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // Add services to be used in here (I don't think we need any)
         public void ConfigureServices(IServiceCollection services)
         {
 
             services.AddControllers();
-            /*services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "IO_Game", Version = "v1" });
-            });*/
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // Configure what to do when we recieve a request. This basically defines the order in which 
+        // things need to be done to the request.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // If we're still coding this then...
             if (env.IsDevelopment())
             {
+                // Give us nice error messages that tell us what went wrong.
                 app.UseDeveloperExceptionPage();
-                //app.UseSwagger();
-                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "IO_Game v1"));
             }
 
+            // WebSockets are useful maybe...
             app.UseWebSockets();
 
-            app.Use(async (context, next) =>
+            // I think this makes http go to https???
+            app.UseHttpsRedirection();
+            
+            // Allows us to redirect users based on whatever, such as redirecting users trying to access a
+            // non-existing game to an error page.
+            app.UseRouting();
+
+            // I think this just means that the client has to be a good boy.
+            app.UseAuthorization();
+
+            // This is where we define all of our redirecting, or "Routing"
+            app.UseEndpoints(endpoints =>
             {
-                if (context.Request.Path.ToString().Contains("/game"))
-                {
+                // If the user is trying to go to /game or something like that
+                endpoints.MapGet("/game", async context => {
+                    // No cache (due to the constant changing between error pages and game pages etc)
                     context.Response.Headers.Add("Cache-Control", "no-cache");
                     context.Response.Headers.Add("Pragma", "no-cache");
+                    // If the game doesn't exist
                     if (!context.Request.QueryString.HasValue || !Server.GameExists(context.Request.QueryString.ToString()[1..]))
                     {
+                        // Write to the response based on the contents of this -v error page.
                         StreamReader sr = File.OpenText(env.WebRootPath + "/errors/GAME_NOT_FOUND.html");
                         string strContents = sr.ReadToEnd();
-                        //To display normal raw contents
                         await context.Response.WriteAsync(strContents);
                         sr.Close();
                     }
+                    // If it is a request to start a WebSocket then...
                     else if (context.WebSockets.IsWebSocketRequest)
                     {
+                        // Accept the request
                         WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                        // And pass it over to our amazing friend, the class Game
                         Server.GetGame(context.Request.QueryString.ToString()[1..]).AddPlayer(webSocket);
                     }
-                    else await next();
-                }
-                else await next();
+                    else
+                    {
+                        // If its a valid game and not a WebSocket, then we can safely send the standard page.
+                        StreamReader sr = File.OpenText(env.WebRootPath + "/game/index.html");
+                        string strContents = sr.ReadToEnd();
+                        await context.Response.WriteAsync(strContents);
+                        sr.Close();
+                    }
+                });
             });
 
+            // This means that if nothing else happens then just use the normal file-system
             app.UseDefaultFiles();
             app.UseStaticFiles();
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-
-        }
-        private static async Task SocketHandler(HttpContext context, WebSocket webSocket)
-        {
-            var buffer = new byte[1024 * 4];
-            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-            while (!result.CloseStatus.HasValue)
-            {
-                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
-
-                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-            }
-            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
         }
     }
 }
